24290defd102bda313203bc88762de828d564d41
kuwata0037 <kuwata0037@gmail.com>
2025-02-28 22:41:29 +0900

    Implement CRUD operations for Todo items in the database repository

--- books/Webアプリ開発で学ぶRust言語入門/my-todo/src/repositories.rs
+++ books/Webアプリ開発で学ぶRust言語入門/my-todo/src/repositories.rs
@@ -6,7 +6,7 @@ use std::{
 use anyhow::Context;
 use axum::async_trait;
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::{prelude::FromRow, PgPool};
 use thiserror::Error;
 use validator::Validate;
 
@@ -14,9 +14,11 @@ use validator::Validate;
 pub enum RepositoryError {
     #[error("Not Found, id is {0}")]
     NotFound(i32),
+    #[error("Unexpected Error: [{0}]")]
+    Unexpected(String),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, FromRow, Serialize, Deserialize)]
 pub struct Todo {
     id: i32,
     text: String,
@@ -65,6 +67,7 @@ pub struct TodoRepositoryForMemory {
 }
 
 impl TodoRepositoryForMemory {
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Self {
             store: Arc::default(),
@@ -142,23 +145,78 @@ impl TodoRepositoryForDb {
 #[async_trait]
 impl TodoRepository for TodoRepositoryForDb {
     async fn create(&self, payload: CreateTodo) -> anyhow::Result<Todo> {
-        todo!()
+        let todo = sqlx::query_as::<_, Todo>(
+            r#"
+                INSERT INTO todos (text, completed)
+                VALUES ($1, false)
+                RETURNING *;
+            "#,
+        )
+        .bind(payload.text.clone())
+        .fetch_one(&self.pool)
+        .await?;
+        Ok(todo)
     }
 
     async fn find(&self, id: i32) -> anyhow::Result<Todo> {
-        todo!()
+        let todo = sqlx::query_as::<_, Todo>(
+            r#"
+                SELECT * FROM todos WHERE id=$1;
+            "#,
+        )
+        .bind(id)
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFound(id),
+            _ => RepositoryError::Unexpected(e.to_string()),
+        })?;
+        Ok(todo)
     }
 
     async fn all(&self) -> anyhow::Result<Vec<Todo>> {
-        todo!()
+        let todos = sqlx::query_as(
+            r#"
+                SELECT * FROM todos ORDER BY id DESC;
+            "#,
+        )
+        .fetch_all(&self.pool)
+        .await?;
+        Ok(todos)
     }
 
     async fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
-        todo!()
+        let old_todo = self.find(id).await?;
+        let todo = sqlx::query_as::<_, Todo>(
+            r#"
+                UPDATE todos SET text=$1, completed=$2
+                WHERE id=$3
+                RETURNING *;
+            "#,
+        )
+        .bind(payload.text.unwrap_or(old_todo.text))
+        .bind(payload.completed.unwrap_or(old_todo.completed))
+        .bind(id)
+        .fetch_one(&self.pool)
+        .await?;
+        Ok(todo)
     }
 
     async fn delete(&self, id: i32) -> anyhow::Result<()> {
-        todo!()
+        sqlx::query(
+            r#"
+                DELETE FROM todos WHERE id=$1;
+            "#,
+        )
+        .bind(id)
+        .execute(&self.pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFound(id),
+            _ => RepositoryError::Unexpected(e.to_string()),
+        })?;
+
+        Ok(())
     }
 }
 
